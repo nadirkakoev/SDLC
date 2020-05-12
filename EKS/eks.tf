ea0c5b676da64a3d9cd9c2fb44e32310401b3dd9
nadirkakoev <nadir_kakoev@yahoo.com>
2020-05-12 23:54:30 +0000

    dss

--- EKS/eks.tf
+++ EKS/eks.tf
@@ -33,3 +33,6 @@ module "my-cluster" {
     }
   ]
 }
+
+
+
---
+++ EKS/output.tf
@@ -0,0 +1,9 @@
+output "ARN" {
+  value = module.my-cluster.cluster_arn
+}
+output "ID" {
+  value = module.my-cluster.cluster_id
+}
+
+
+
