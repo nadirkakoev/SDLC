74477a2e46b227a347ae05d3820fed5c9db1463b
nadirkakoev <nadir_kakoev@yahoo.com>
2020-05-13 01:55:49 +0000

    hj

--- EKS/backend.tf
+++ EKS/backend.tf
@@ -1,7 +1,7 @@
-# terraform {
-#   backend "s3" {
-#     bucket = "mybucket"
-#     key    = "path/to/my/key"
-#     region = "us-east-1"
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket = "state-class-nadir"
+    key    = "path/to/my/key"
+    region = "us-east-1"
+  }
+}
